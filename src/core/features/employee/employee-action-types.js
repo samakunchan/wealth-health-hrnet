5c2ec5d8cb46771406df34a377fb3ae072d85f13
samakunchan <badgehades@hotmail.com>
2024-05-21 15:03:24 +0200

    feat: employee structure
    - Envoie de la gestion des employées avec redux

---
+++ src/core/features/employee/employee-action-types.js
@@ -0,0 +1,11 @@
+export const AUTH_SLICE_NAME = 'Authentication slice';
+export const AUTH_LOGIN_NAME = 'Auth/login';
+export const AUTH_LOGOUT_NAME = 'Auth/logout';
+export const AUTH_IS_IDLE = 'Auth is idle';
+export const AUTH_IS_LOADING = 'Auth is loading';
+export const AUTH_IS_SUCCEEDED = 'Auth is provided';
+export const AUTH_IS_REJECT = 'Auth is not provided';
+export const AUTH_TOKEN_DEFAULT_MESSAGE = '[Need access token]';
+export const AUTH_TOKEN_LOCAL_STORAGE_MESSAGE = '';
+export const AUTH_LOCALSTORAGE_NAME = 'access-token';
+export const ERROR_MESSAGE = '[no error for now]';
